9514ae446e103674acef38018ccb439e2a3f4668
dedie@CALLMT22223 <daniel.edie@gettyimages.com>
2023-11-08 16:44:59 -0700

    12,13

---
+++ 12-state-management/state-management.tf
@@ -0,0 +1,37 @@
+## This snippet is from the Terraform State Management video.
+
+## state-management.tf
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "YOUR-ACCESS-KEY"
+  secret_key = "YOUR-SECRET-KEY"
+}
+
+resource "aws_instance" "myec2" {
+  ami           = "ami-082b5a644766e0e6f"
+  instance_type = "t2.micro"
+}
+
+resource "aws_iam_user" "lb" {
+  name = "loadbalancer"
+  path = "/system/"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "kplabs-remote-backends"
+    key    = "demo.tfstate"
+    region = "us-east-1"
+    access_key = "YOUR-ACCESS-KEY"
+    secret_key = "YOUR-SECRET-KEY"
+  }
+}
+
+
+## Commands used for State Management 
+
+
+# terraform state list
+# terraform state mv aws_instance.webapp aws_instance.myec2
+# terraform state pull
+# terraform state rm aws_instance.myec2
